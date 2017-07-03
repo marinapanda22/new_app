e5d9f5ef234f3c270e4b983ef0eebcb521b80c14
Smagina Marina <pandamarina736@gmail.com>
2017-07-03 23:09:25 +0600

    Добавлены сиды

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 # Создание пользователей
-admin = User.create!(email: 'admin@example.com', password: 'password', role: 'admin', name: 'Admin')
+admin = User.create!(email: 'admin@example.com', password: 'password', role: 'admin', name: Faker::HarryPotter.character)
 admin.save
-user = User.create!(email: 'user@example.com', password: 'password', role: 'none', name: 'User')
+user = User.create!(email: 'user@example.com', password: 'password', role: 'none', name: Faker::HarryPotter.character)
 user.save
+
+
+dir = Rails.root + 'app' + 'assets' + 'images'
+10.times do
+  Post.create(title: "Beautiful nature", image:File.new(dir + "5.jpg"),
+                 user_id: 1
+  )
+end
+10.times do
+  Post.create(title: "Lovely panda", image:File.new(dir + "24.jpg"),
+                 user_id: 2
+  )
+end
